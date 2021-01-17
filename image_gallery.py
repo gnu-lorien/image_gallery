114c29f971eeeefe5c309b3a30df2d7638038fa9
Andrew Sayman <andrew.sayman@gmail.com>
2021-01-16 20:49:50 -0500

    Gallery breaks up into smaller files and creates an index

--- image_gallery.py
+++ image_gallery.py
@@ -1,10 +1,12 @@
 # encoding: utf-8
 
 import os
+from itertools import zip_longest
 import sys
+import argparse
 
 def str2file(filepath, s):
-    with open(filepath, encoding='utf8', mode='w') as f:
+    with open(filepath, mode='w') as f:
         f.write(s)
 
 def get_filelist(basedir):
@@ -15,52 +17,81 @@ def get_filelist(basedir):
             ret.append(fullpath)
     return ret
 
-MYFULLPATH = os.path.abspath(sys.argv[0])
-MYDIR = os.path.dirname(MYFULLPATH)
 
-filenames_all = get_filelist(MYDIR)
-filenames_jpg = [filename for filename in filenames_all if filename.lower()[-4:]=='.jpg']
-filenames_curdir_removed = [filename.replace('{:}\\'.format(MYDIR), '') for filename in filenames_jpg]
-filenames_slash_delim = [filename.replace('\\', '/') for filename in filenames_curdir_removed]
+conf_image_x = 500
+conf_image_y = 500
+conf_image_count_per_line = 3
+conf_images_per_page = 50
 
-print(filenames_slash_delim)
-print('{:} items.'.format(len(filenames_jpg)))
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
-outfilename = 'image_gallery.html'
-outstrs = ''
+galleries_generated = []
 
-conf_image_x = 150
-conf_image_y = 150
-conf_image_count_per_line = 6
+def generate_gallery_with_index(rootdir):
+    filenames_all = get_filelist(rootdir)
+    filenames_jpg = [filename for filename in filenames_all if filename.lower()[-4:] == '.jpg']
+    filenames_curdir_removed = [filename.replace('{:}\\'.format(rootdir), '') for filename in filenames_jpg]
+    filenames_slash_delim = [filename.replace('\\', '/') for filename in filenames_curdir_removed]
 
-for i,image_filepath in enumerate(filenames_slash_delim):
-    url = image_filepath
-    alttext = url
+    print(filenames_slash_delim)
+    print('{:} items.'.format(len(filenames_jpg)))
+    for pagecount, page in enumerate(grouper(filenames_slash_delim, conf_images_per_page)):
+        outstrs = ''
+        for i,image_filepath in enumerate(page):
+            url = image_filepath
+            alttext = url
 
-    image_html_template = '<a href="{href}" target="_blank"><img src="{src}" width="{width}px" height="{height}px"></a>'
-    image_kwargs = {
-        'href'   : url,
-        'src'    : url,
-        'width'  : conf_image_x,
-        'height' : conf_image_y,
-    }
+            image_html_template = '<a href="{href}" target="_blank"><img src="{src}" width="{width}px" height="{height}px"></a>'
+            image_kwargs = {
+                'href'   : url,
+                'src'    : url,
+                'width'  : conf_image_x,
+                'height' : conf_image_y,
+            }
 
-    image_html = image_html_template.format(**image_kwargs)
+            image_html = image_html_template.format(**image_kwargs)
 
-    outstrs += image_html
-    outstrs += '\n'
-    if i%conf_image_count_per_line==(conf_image_count_per_line-1):
-        outstrs += '<br>\n'
+            outstrs += image_html
+            outstrs += '\n'
+            if i%conf_image_count_per_line==(conf_image_count_per_line-1):
+                outstrs += '<br>\n'
 
-html_template = """<html>
-<head>
-    <title>Image Gallery</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-{:}
-</body>
-</html>"""
+        html_template = """<html>
+        <head>
+            <title>Image Gallery</title>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+        {:}
+        </body>
+        </html>"""
 
-html = html_template.format(outstrs)
-str2file(outfilename, html)
+        html = html_template.format(outstrs)
+        basename = "image_gallery_{}.html".format(pagecount)
+        outfilename = os.path.join(rootdir, basename)
+        str2file(outfilename, html)
+        galleries_generated.append(basename)
+
+    index_template = """<html>
+        <head>
+            <title>Image Galleries</title>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+        {:}
+        </body>
+        </html>"""
+
+    gallery_links = []
+    for gallery in galleries_generated:
+        gallery_links.append("<a href=\"{}\">Gallery {}</a>".format(gallery, gallery))
+    html = index_template.format("<br/>".join(gallery_links))
+    str2file(os.path.join(rootdir, "index.html"), html)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("rootdir")
+    args = parser.parse_args()
+    generate_gallery_with_index(args.rootdir)
